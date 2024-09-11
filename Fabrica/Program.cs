2a40501bb8f8745d379d1d8b005417c5b6ede37b
dariogas <dario_xd@hotmail.com>
2024-09-11 13:38:52 -0300

    Practica Switch

--- Fabrica/Program.cs
+++ Fabrica/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Primer saludo");
+Console.WriteLine("Hoy realizando un par de pruebas");
+
+//Ejemplo de calculadora
+
+double num1 = 4;
+double num2 = 5;
+string operacion = "multiplicacion";
+double resultado;
+
+
+
+switch (operacion.ToLower())
+{
+	case "suma":
+		resultado = num1 + num2;
+        Console.WriteLine($"El resultado de la operacion es: {resultado}");
+        break;
+	case "resta":
+		resultado = num1 - num2;
+        Console.WriteLine($"El resultado de la operacion es: {resultado}");
+
+        break;
+	case "multiplicacion":
+		resultado = num1 * num2;
+        Console.WriteLine($"El resultado de la operacion es: {resultado}");
+
+        break;
+	case "division":
+		resultado = num1 / num2;
+        Console.WriteLine($"El resultado de la operacion es: {resultado}");
+
+        break;
+	default:
+		Console.WriteLine("Elija una opcion correcta");
+		break;
+}
